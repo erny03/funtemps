8053278d8db9fbd9caa5b6c65f851575d8447498
erny03 <erikny03@gmail.com>
2023-02-23 17:45:41 +0100

    lagt inn manglende tester i conv_test.go

--- conv/conv_test.go
+++ conv/conv_test.go
@@ -32,5 +32,92 @@ func TestFarhenheitToCelsius(t *testing.T) {
 	}
 }
 
-// De andre testfunksjonene implementeres her
-// ...
+func TestFarhenheitToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: -297.40, want: 273.15},
+	}
+
+	for _, tc := range tests {
+		got := FarhenheitToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestCelsiusToFarhenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 56.70, want: 134.06},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToFarhenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 15000000, want: 15000273.15},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestKelvinToFarhenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 5778, want: 5536.85},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToFarhenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 329.82, want: 56.67},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToCelsius(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
